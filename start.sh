ab114b70d13b2adcb55d6d32d9b9e50e208b0777
farhad <37184794+farhad312@users.noreply.github.com>
2025-08-31 14:52:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-service xinetd restart
-service ssh restart
-exec $@
+
+while true; do
+    echo "Input number:"
+    echo "1 - LS"
+    echo "2 - Clear"
+    echo "3 - Ping"
+    echo "0 - Exit"
+    read -r choice
+
+    case $choice in
+        1)
+            ls
+            ;;
+        2)
+            clear
+            ;;
+        3)
+            ansible -i inventory.ini myhosts -m ping --ask-pass
+
+            ;;
+        0)
+            echo "Exit"
